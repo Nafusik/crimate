2344d303c14b5443bfa35886fe012f028972b1fc
Francesca Koulikov <fkoulikov@illuminateed.net>
2019-11-12 13:08:23 -0500

    fixing some error due to missing links

--- crimate/Model/CrimeRateDataModel.swift
+++ crimate/Model/CrimeRateDataModel.swift
@@ -20,38 +20,17 @@ class CrimeRateDataModel {
         
         switch (incidentTotal) {
             
-        case 0...300 :
-            return "tstorm1"
+        case 0...10 :
+            return "black"
             
-        case 301...500 :
-            return "light_rain"
+        case 11...50 :
+            return "embarrassed"
             
-        case 501...600 :
-            return "shower3"
+        case 51...100 :
+            return "scared"
             
-        case 601...700 :
-            return "snow4"
-            
-        case 701...771 :
-            return "fog"
-            
-        case 772...799 :
-            return "tstorm3"
-            
-        case 800 :
-            return "sunny"
-            
-        case 801...804 :
-            return "cloudy2"
-            
-        case 900...903, 905...1000  :
-            return "tstorm3"
-            
-        case 903 :
-            return "snow5"
-            
-        case 904 :
-            return "sunny"
+        case 100...1000 :
+            return "fire"
             
         default :
             return "dunno"
