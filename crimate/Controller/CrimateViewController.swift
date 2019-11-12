cd557d939d8274aaca189f2bf415d45abccfdfdb
Francesca Koulikov <fkoulikov@illuminateed.net>
2019-11-12 12:00:46 -0500

    adding template for board

---
+++ crimate/Controller/CrimateViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CrimateViewController.swift
+//  crimate
+//
+//  Created by Francesca Koulikov on 11/12/19.
+//  Copyright © 2019 Francesca Koulikov. All rights reserved.
+//
+
+import Foundation
